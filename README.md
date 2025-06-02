2fb8c350afc76cb7681b281c9c20d9ab047afd10
Yash Puneet <yash.puneet02@gmail.com>
2025-06-02 11:45:10 -0700

    Added Dataloader and Visualization

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ECE_285_Project
+# ECE_285_Project: Vehicle Detection
+
+Dataset Link: https://drive.google.com/drive/u/1/folders/14kFoqngnCyV2Bdb_hlsEj6LJrtIlNbYC
---
+++ dataset/img_to_npz.py
@@ -0,0 +1,48 @@
+from PIL import Image
+import os, sys
+import cv2
+import numpy as np
+
+def load_dataset():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    path_vehicle = os.path.join(base_dir, "data", "vehicles")
+    path_notvehicle = os.path.join(base_dir, "data", "non-vehicles")
+    dirs_vehicle = os.listdir(path_vehicle)
+    dirs_notvehicle = os.listdir(path_notvehicle)
+    dirs_vehicle.sort()
+    dirs_notvehicle.sort()
+    data=[]
+    labels = []
+
+    # Label = 1 if vehicle
+    for item in dirs_vehicle:
+        full_path = os.path.join(path_vehicle, item)
+        if os.path.isfile(full_path):
+            im = Image.open(full_path).convert("RGB")
+            im = np.array(im)
+            data.append(im)
+            labels.append(1)
+
+    for item in dirs_notvehicle:
+        full_path = os.path.join(path_notvehicle, item)
+        if os.path.isfile(full_path):
+            im = Image.open(full_path).convert("RGB")
+            im = np.array(im)
+            data.append(im)
+            labels.append(0)
+    
+    labels = np.array(labels)
+
+    return data, labels
+
+if __name__ == "__main__":
+    
+    data, labels = load_dataset()
+    data = np.array(data)
+    labels = np.array(labels)
+    print(data.shape)
+    print(labels.shape)
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    save_path = os.path.join(script_dir, "vehicle_detection_dataset.npz")
+    np.savez(save_path, images=data, labels=labels)
