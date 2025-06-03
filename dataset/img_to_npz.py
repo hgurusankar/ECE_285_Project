608b5807b4ac19c92ea754026e7f7f7e372901ea
Yash Puneet <yash.puneet02@gmail.com>
2025-06-02 21:48:37 -0700

    Updated for new dataset + typo fixes

--- dataset/img_to_npz.py
+++ dataset/img_to_npz.py
@@ -1,35 +1,44 @@
-from PIL import Image
-import os, sys
-import cv2
+from PIL import Image, ImageFile
+import os
 import numpy as np
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def load_dataset():
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    path_vehicle = os.path.join(base_dir, "data", "vehicles")
-    path_notvehicle = os.path.join(base_dir, "data", "non-vehicles")
-    dirs_vehicle = os.listdir(path_vehicle)
-    dirs_notvehicle = os.listdir(path_notvehicle)
-    dirs_vehicle.sort()
-    dirs_notvehicle.sort()
+
+    # Class image paths
+    path_van = os.path.join(base_dir, "data", "van") # 1
+    path_truck = os.path.join(base_dir, "data", "truck") # 2
+    path_tractor = os.path.join(base_dir, "data", "tractor") # 3
+    path_bus = os.path.join(base_dir, "data", "bus") # 4
+    path_auto = os.path.join(base_dir, "data", "auto") # 5
+
+    paths = [path_van, path_truck, path_tractor, path_bus, path_auto]
+    target_size = (64, 64)
+
+    # Open Dirs
+    dirs = []
+    for path in paths:
+        dir = os.listdir(path)
+        dir.sort()
+        dirs.append(dir)
+    
     data=[]
     labels = []
 
-    # Label = 1 if vehicle
-    for item in dirs_vehicle:
-        full_path = os.path.join(path_vehicle, item)
-        if os.path.isfile(full_path):
-            im = Image.open(full_path).convert("RGB")
-            im = np.array(im)
-            data.append(im)
-            labels.append(1)
+    for i in range(len(dirs)):
+        path = paths[i]
+        dir = dirs[i]
 
-    for item in dirs_notvehicle:
-        full_path = os.path.join(path_notvehicle, item)
-        if os.path.isfile(full_path):
-            im = Image.open(full_path).convert("RGB")
-            im = np.array(im)
-            data.append(im)
-            labels.append(0)
+        for item in dir:
+            full_path = os.path.join(path, item)
+            if os.path.isfile(full_path):
+                im = Image.open(full_path).convert("RGB")
+                im = im.resize(target_size)
+                im = np.array(im) / 255.0
+                data.append(im)
+                labels.append(i)
     
     labels = np.array(labels)
 
